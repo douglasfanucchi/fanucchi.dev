0d67d8ddccf7a5618ebb3146eee2707de1eba5a5
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-01-14 10:49:30 -0300

    implementa listagem de categorias

--- inc/template-part/projects.php
+++ inc/template-part/projects.php
@@ -1,3 +1,7 @@
-<section class="projects">
-    
+<section class="projects container">
+    <ul class="projects__categories">
+        <li class="projects__category"><button class="button active">All</button></li>
+        <li data-category="plugins" class="projects__category"><button class="button">Plugins</button></li>
+        <li data-category="themes" class="projects__category"><button class="button">Themes</button></li>
+    </ul>
 </section>
--- src/js/components/projects.js
+++ src/js/components/projects.js
@@ -1,5 +1,7 @@
 export default class Projects {
     constructor($element) {
-        this.$element = $element
+        this.$element    = $element
+        this.$categories = this.$element.querySelector(".projects__categories")
+        this.$list       = this.$element.querySelector(".projects__list")
     }
 }
