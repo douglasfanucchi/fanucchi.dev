8f9169919d955cef1242b9c1dfceec0bb28a22b2
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-01-14 12:45:38 -0300

    muda elemento que recebe classe "active"

--- inc/template-part/projects.php
+++ inc/template-part/projects.php
@@ -1,6 +1,6 @@
 <section class="projects container">
     <ul class="projects__categories">
-        <li class="projects__category"><button class="button active">All</button></li>
+        <li class="projects__category active"><button class="button">All</button></li>
         <li data-category="plugins" class="projects__category"><button class="button">Plugins</button></li>
         <li data-category="themes" class="projects__category"><button class="button">Themes</button></li>
     </ul>
