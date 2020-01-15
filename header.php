b1bfd30c55dfe8bb247051ea080fc98f8f33eef0
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-01-15 19:25:52 -0300

    retira zoom de mobile

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no;">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700&display=swap&subset=latin-ext" rel="stylesheet">
     <?php wp_head(); ?>
