d27d389a45761f81f5b5d4ed2007abe79381606f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-01-27 20:12:38 -0300

    implementa footer

--- footer.php
+++ footer.php
@@ -1,4 +1,6 @@
-
+    <footer class="footer">
+        <p class="footer__copyright">Desenvolvidor por <a target="_blank" href="https://github.com/douglasfanucchi">Fanucchi</a></p>
+    </footer>
     <?php wp_footer(); ?>
 </body>
 </html>
