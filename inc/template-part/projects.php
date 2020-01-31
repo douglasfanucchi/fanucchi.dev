b8a3bcdfed0eb7be7e394e869d030bea5223a69f
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-01-31 15:43:43 -0300

    adiciona area de conteudo em project list

--- inc/template-part/projects.php
+++ inc/template-part/projects.php
@@ -6,40 +6,47 @@
     </ul>
     <ul class="projects__list">
         <li class="projects__item" data-category="plugins">
-            <img src="https://i.picsum.photos/id/765/390/270.jpg" alt="1">
+            <figure class="item__thumb">
+                <img src="https://i.picsum.photos/id/765/390/270.jpg" alt="1">
+            </figure>
+            <div class="item__content">
+                <h3 class="item__title">Meu projeto</h3>
+                <p class="item__description">
+                    Plugin que integra mercado pago com funcionalidade de assinatura!
+                </p>
+                <ul class="item__tecnologies">
+                    <h3 class="title">Tecnologias: </h3>
+                    <li class="item__tecnology">PHP</li>
+                    <li class="item__tecnology">JS</li>
+                    <li class="item__tecnology">MySQL</li>
+                    <li class="item__tecnology">JavaScript</li>
+                </ul>
+            </div>
         </li>
         <li class="projects__item" data-category="themes">
-            <img src="https://i.picsum.photos/id/825/360/360.jpg" alt="2">
+            <figure class="item__thumb">
+                <img src="https://i.picsum.photos/id/825/360/360.jpg" alt="2">
+            </figure>
         </li>
         <li class="projects__item" data-category="plugins">
-            <img src="https://i.picsum.photos/id/768/360/238.jpg" alt="3">
+            <figure class="item__thumb">
+                <img src="https://i.picsum.photos/id/768/360/238.jpg" alt="3">
+            </figure>
         </li>
         <li class="projects__item" data-category="plugins">
-            <img src="https://i.picsum.photos/id/690/360/240.jpg" alt="4">
+            <figure class="item__thumb">
+                <img src="https://i.picsum.photos/id/690/360/240.jpg" alt="4">
+            </figure>
         </li>
         <li class="projects__item" data-category="themes">
-            <img src="https://i.picsum.photos/id/237/360/240.jpg" alt="5">
+            <figure class="item__thumb">
+                <img src="https://i.picsum.photos/id/237/360/240.jpg" alt="5">
+            </figure>
         </li>
         <li class="projects__item" data-category="themes">
-            <img src="https://i.picsum.photos/id/289/360/218.jpg" alt="6">
-        </li>
-        <li class="projects__item" data-category="themes">
-            <img src="https://i.picsum.photos/id/227/360/240.jpg" alt="5">
-        </li>
-        <li class="projects__item" data-category="plugins">
-            <img src="https://i.picsum.photos/id/219/360/218.jpg" alt="6">
-        </li>
-        <li class="projects__item" data-category="plugins">
-            <img src="https://i.picsum.photos/id/297/360/240.jpg" alt="5">
-        </li>
-        <li class="projects__item" data-category="plugins">
-            <img src="https://i.picsum.photos/id/259/360/218.jpg" alt="6">
-        </li>
-        <li class="projects__item" data-category="themes">
-            <img src="https://i.picsum.photos/id/259/360/218.jpg" alt="6">
-        </li>
-        <li class="projects__item" data-category="themes">
-            <img src="https://i.picsum.photos/id/259/360/218.jpg" alt="6">
+            <figure class="item__thumb">
+                <img src="https://i.picsum.photos/id/289/360/218.jpg" alt="6">
+            </figure>
         </li>
     </ul>
 </section>
