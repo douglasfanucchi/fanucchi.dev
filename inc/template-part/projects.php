cb82a32ff41bfb82ca1c203e96c2562c5bbb2b52
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-07-07 16:31:33 -0300

    Altera texto do botão

--- inc/template-part/projects.php
+++ inc/template-part/projects.php
@@ -42,7 +42,7 @@
                         <li class="item__tecnology"><?php echo $tecnologia; ?></li>
                         <?php endforeach; ?>
                     </ul>
-                    <button data-post-id="<?php echo $post->ID; ?>" class="item__read-more">Saiba mais</button>
+                    <button data-post-id="<?php echo $post->ID; ?>" class="item__read-more">Acessar</button>
                 </div>
             </li>
             <?php
