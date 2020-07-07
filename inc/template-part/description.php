edfecd8e04202eec10abea117d46c98b897988ca
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-07-07 15:00:28 -0300

    Ajusta texto da descrição

--- inc/template-part/description.php
+++ inc/template-part/description.php
@@ -1,5 +1,5 @@
 <section class="description">
     <div class="container">
-        <h2 class="description__text">Especialista em desenvolvimento de plugins e temas para WordPress com vasto conhecimento em <span data-words="PHP|MySQL|HTML5|CSS3|JavaScript" class="description__highlight"></span></h2>
+        <h2 class="description__text">Especialista em desenvolvimento de plugins e temas para WordPress com conhecimento em <span data-words="PHP|MySQL|HTML5|CSS3|JavaScript" class="description__highlight"></span></h2>
     </div>
 </section>
