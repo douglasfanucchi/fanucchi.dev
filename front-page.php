fa023b0c02e218bf2f213a67834046e009cc8d56
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-07-08 02:30:44 -0300

    Inclui seção de depoimentos

--- front-page.php
+++ front-page.php
@@ -2,10 +2,5 @@
 get_header();
     get_template_part('inc/template-part/description');
 	get_template_part('inc/template-part/projects');
-
-	if( have_posts() )
-		while( have_posts() ){
-			the_post();
-			the_content();
-		}
+	get_template_part('inc/template-part/qualifications');
 get_footer();
