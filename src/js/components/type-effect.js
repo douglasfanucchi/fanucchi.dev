fdd45c742933aa571dc9f8c51310a892d2e08833
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-09-04 12:11:38 -0300

    Prettier on TypeEffect

--- src/js/components/type-effect.js
+++ src/js/components/type-effect.js
@@ -1,35 +1,35 @@
 export default class TypeEffect {
-    constructor($element, speed = 100) {
-        this.$element   = $element
-        this.speed      = speed
-        this.words      = this.$element.getAttribute("data-words").split("|")
-    }
+  constructor($element, speed = 100) {
+    this.$element = $element;
+    this.speed = speed;
+    this.words = this.$element.getAttribute('data-words').split('|');
+  }
 
-    async printWords() {
-        if( !this.words) return
+  async printWords() {
+    if (!this.words) return;
 
-        this.$element.innerHTML = ""
+    this.$element.innerHTML = '';
 
-        await this.print()
-        this.printWords()
-    }
+    await this.print();
+    this.printWords();
+  }
 
-    print() {
-        const word    = this.words.shift()
-        const letters = word.split("")
+  print() {
+    const word = this.words.shift();
+    const letters = word.split('');
 
-        this.words.push(word)
+    this.words.push(word);
 
-        return new Promise(resolve => {
-            const interval = setInterval(() => {
-                if( letters.length === 0 ) {
-                    clearInterval(interval)
-                    setTimeout( () => resolve(), 1000 )
-                    return
-                }
+    return new Promise((resolve) => {
+      const interval = setInterval(() => {
+        if (letters.length === 0) {
+          clearInterval(interval);
+          setTimeout(() => resolve(), 1000);
+          return;
+        }
 
-                this.$element.innerHTML += letters.shift()
-            }, this.speed)
-        })
-    }
+        this.$element.innerHTML += letters.shift();
+      }, this.speed);
+    });
+  }
 }
