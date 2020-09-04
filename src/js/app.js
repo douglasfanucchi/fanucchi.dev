b259633d9ec4390b6a5d8b377f3c4aca995a2262
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-09-04 12:54:02 -0300

    Prettier on app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,69 +1,71 @@
-import Body from "./components/body"
-import Hamburger from "./components/hamburger"
-import TypeEffect from "./components/type-effect"
-import { Projects, ProjectsCategories, ProjectsList } from "./components/projects"
-import "slick-slider"
-import Modal from "./components/modal"
+import Body from './components/body';
+import Hamburger from './components/hamburger';
+import TypeEffect from './components/type-effect';
+import {
+  Projects,
+  ProjectsCategories,
+  ProjectsList,
+} from './components/projects';
+import 'slick-slider';
+import Modal from './components/modal';
 
-const $body   = document.querySelector("body")
-const $header = document.getElementById("header")
-const $hamburger = $header.querySelector(".primary-menu__hamburger-button")
-const $typeEffect = $body.querySelector(".description__highlight")
+const $body = document.querySelector('body');
+const $header = document.getElementById('header');
+const $hamburger = $header.querySelector('.primary-menu__hamburger-button');
+const $typeEffect = $body.querySelector('.description__highlight');
 
-const body = new Body($body)
+const body = new Body($body);
 
 window.addEventListener('load', () => {
-    body.setMarginTop($header)
-    
-    if($hamburger) 
-        new Hamburger($hamburger)
-    
-    if( $typeEffect )
-        new TypeEffect($typeEffect).printWords()
+  body.setMarginTop($header);
 
-    const $projects = $body.querySelector(".projects");
-    let $projectsCategories = undefined
-    let $modal = undefined
-    let $projectsList = undefined
+  if ($hamburger) new Hamburger($hamburger);
 
-    if( $projects ) {
-        $projectsCategories = $projects.querySelector(".projects__categories")
-        $projectsList = $projects.querySelector(".projects__list")
-        $modal = $projects.querySelector('.modal');
+  if ($typeEffect) new TypeEffect($typeEffect).printWords();
 
-        const {state: ProjectsState} = new Projects($projects)
-        new ProjectsCategories($projectsCategories, ProjectsState)
-        new ProjectsList($projectsList, ProjectsState)
-        const {state: ModalState} = new Modal($modal)
+  const $projects = $body.querySelector('.projects');
+  let $projectsCategories = undefined;
+  let $modal = undefined;
+  let $projectsList = undefined;
 
-        // $projectsList.querySelectorAll('.item__content').forEach( $itemContent => {
-        //     const $button = $itemContent.querySelector(".item__read-more") 
-        
-        //     if(!$button || !$modal) return
-        
-        //     const button = new ReadMore($button, ModalState)
-            
-        //     button.setItemToActive( $modal )
-        // })
-    }
+  if ($projects) {
+    $projectsCategories = $projects.querySelector('.projects__categories');
+    $projectsList = $projects.querySelector('.projects__list');
+    $modal = $projects.querySelector('.modal');
 
-    const $qualifications = document.querySelector(".qualifications");
+    const { state: ProjectsState } = new Projects($projects);
+    new ProjectsCategories($projectsCategories, ProjectsState);
+    new ProjectsList($projectsList, ProjectsState);
+    const { state: ModalState } = new Modal($modal);
 
-    if( $qualifications ) {
-        jQuery($qualifications).slick({
-            slidesToShow: 2,
-            vertical: false,
-            arrows: false,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            responsive: [
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        })
-    }
-})
+    // $projectsList.querySelectorAll('.item__content').forEach( $itemContent => {
+    //     const $button = $itemContent.querySelector(".item__read-more")
+
+    //     if(!$button || !$modal) return
+
+    //     const button = new ReadMore($button, ModalState)
+
+    //     button.setItemToActive( $modal )
+    // })
+  }
+
+  const $qualifications = document.querySelector('.qualifications');
+
+  if ($qualifications) {
+    jQuery($qualifications).slick({
+      slidesToShow: 2,
+      vertical: false,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  }
+});
